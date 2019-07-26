77cfbf349169a9085afec60837ebacc65ff3ace3
Maria Lagana <maria.lagana@vdab.be>
2019-07-26 11:02:11 +0000

    test core.autocrlf

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-#######################################################################################
+##############################################################################################
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/files
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests
-#######################################################################################
+##############################################################################################
 
