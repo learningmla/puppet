5922b41179b409c08362d31fecb4f9efa40e70f9
Maria Lagana <maria.lagana@vdab.be>
2019-07-26 08:22:35 +0000

    Test distribution

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
+#
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/files/
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests/
