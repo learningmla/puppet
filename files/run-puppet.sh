5496b6443cb9e9188f266399a407e81928f8a4ec
Maria Lagana <maria.lagana@vdab.be>
2019-07-26 14:08:44 +0000

    correction

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
 ##############################################################################################
-vi +':w ++ff=unix' +':q' "/etc/puppetlabs/code/environments/production/files/run-puppet.sh"
-vi +':w ++ff=unix' +':q' "/usr/local/bin/run-puppet"
-##############################################################################################
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/files
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests
 ##############################################################################################
+/home/vagrant/mla/t.cmd
