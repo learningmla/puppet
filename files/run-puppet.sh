863d84b1857adab47244eff9ef910d803724609b
Maria Lagana <maria.lagana@vdab.be>
2019-07-26 09:34:42 +0000

    corrections special characters

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd /etc/puppetlabs/code/environments/production && git pull
-#
+#######################################################################################
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/manifests
 /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environments/production/files
+#######################################################################################
+
